f28ea1cd1f2ff3527c1b4726ba8d60e0ced1bbb8
Ace <114063817+lugh-tuatha@users.noreply.github.com>
2023-10-29 15:40:56 +0800

    create: hero section in home

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				background: '#F3F3F3',
+				primary: '#FC7C5F',
+				primary80: 'rgba(252, 124, 95, 0.8)',
+			}
+		},
 	},
 	plugins: [],
 }
